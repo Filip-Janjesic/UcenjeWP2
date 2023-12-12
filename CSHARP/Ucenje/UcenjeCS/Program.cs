c1fbe0719a68cafb13a68929495020c790f28fa6
Filip Janješić <filip.janjesic@gmail.com>
2023-12-12 18:20:50 +0100

    Update Program.cs

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,5 +1,16 @@
-﻿Console.WriteLine(7);
-Console.WriteLine(true);
-Console.Write("Prvi,");
-Console.Write("Drugi i ");
-Console.WriteLine(3.14);
+﻿//Console.WriteLine(7);
+//Console.WriteLine(true);
+//Console.Write("Prvi,");
+//Console.Write("Drugi i ");
+//Console.WriteLine(3.14);
+
+// varijabla je prostor u memoriji
+//Console.Write("Unesi ime: ");
+//string Ime = Console.ReadLine();
+
+//Console.WriteLine("Unijeli ste " + Ime);
+
+Console.Write("Unesi visinu u centimetrima: ");
+int Visina = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Visoki ste " +  (float)Visina / 100 + " metara.");
