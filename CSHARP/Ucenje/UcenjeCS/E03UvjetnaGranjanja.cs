428b456aa793da9a4160b1f1b79af30f810a74c3
Filip-Janjesic <filip.janjesic@gmail.com>
2023-12-19 17:27:25 +0100

    Create E03UvjetnaGranjanja.cs

---
+++ CSHARP/Ucenje/UcenjeCS/E03UvjetnaGranjanja.cs
@@ -0,0 +1,44 @@
+﻿
+namespace UcenjeCS
+{
+    internal class E03UvjetnaGranjanja
+    {
+
+        public static void Izvedi()
+        {
+            int i = 7;
+
+            bool uvjet = i == 7;
+
+            if (uvjet)
+            {
+                Console.WriteLine("Ušao sam u true dio if naredbe");
+            }
+
+            if(i == 7)
+            {
+                Console.WriteLine("Isto kao i prije");
+            }
+
+            if(i < 5)
+            {
+                Console.WriteLine("i je manje od 5");
+            }
+            else
+            {
+                Console.WriteLine("i nije manje od 5");
+            }
+
+            int j = 2;
+            if (i == 7)
+            {
+                if (j==2)
+                {
+                    Console.WriteLine("Oba uvijeta zadovoljena");
+                }
+            }
+
+        }
+    }
+}
+
