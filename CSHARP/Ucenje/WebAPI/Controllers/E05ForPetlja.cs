a651c22b139e7ef879ebd8d8830557610e8848eb
Filip-Janjesic <filip.janjesic@gmail.com>
2023-12-21 18:03:23 +0100

    Create E05ForPetlja.cs

---
+++ CSHARP/Ucenje/WebAPI/Controllers/E05ForPetlja.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("E05")]
+    public class E05ForPetlja:ControllerBase
+    {
+        [HttpGet]
+        [Route("zad1")]
+        public int[] Zad1( int brojevi)
+        {
+            // Ruta vraća niz s brojevima od 1 do brojevi
+            int[] niz = new int[brojevi];
+            for(int i = 0; i < brojevi; i++)
+            {
+                niz[i] = i+1;
+            }
+            
+            return niz;
+            
+        }
+
+
+       
+    }
+}
