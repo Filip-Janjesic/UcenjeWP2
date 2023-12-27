1d3a1b24e397da226af78ba346303a819afeddeb
Filip Janješić <filip.janjesic@gmail.com>
2023-12-27 23:54:36 +0100

    UcenjeCS - Zimski zadaci
    UcenjeCS - Zimski zadaCI
    Kreirati klasu V01ZimskoVjezbanje te slijedno jedno za drugim riješiti sljedeće zadatke
    1. Napisati program koji ispisuje sve brojeve od 1 do 100
    2. Napisati program koji ispisuje sve brojeve od 100 do 1

---
+++ CSHARP/Ucenje/UcenjeCS/V01ZimskoVjezbanje.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace UcenjeCS
+{
+    internal class V01ZimskoVjezbanje
+    {
+        public static void Izvedi()
+        {
+            // Ispis brojeva od 1 do 100
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Ispis brojeva od 100 do 1
+            for (int i = 100; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
