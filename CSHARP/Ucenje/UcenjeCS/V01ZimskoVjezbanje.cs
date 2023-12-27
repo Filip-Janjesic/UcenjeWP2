84b90c62121fe1a28955f20233bec31f556cec65
Filip Janješić <filip.janjesic@gmail.com>
2023-12-28 00:00:15 +0100

    4. Napisati program koji unosi brojeve sve dok ne unese broj veći od 100, a zatim ispisuje koliko je bilo pokušaja unosa

--- CSHARP/Ucenje/UcenjeCS/V01ZimskoVjezbanje.cs
+++ CSHARP/Ucenje/UcenjeCS/V01ZimskoVjezbanje.cs
@@ -18,6 +18,7 @@ namespace UcenjeCS
                 Console.WriteLine(i);
             }
 
+            // 3. Napisati program koji ispisuje sve brojeve od 1 do 100 koji su cjelobrojno djeljivi s 7
             for (int i = 1; i <= 100; i++)
             {
                 if (i % 7 == 0)
@@ -25,6 +26,33 @@ namespace UcenjeCS
                     Console.WriteLine(i);
                 }
             }
+
+            // 4. Napisati program koji unosi brojeve sve dok ne unese broj veći od 100, a zatim ispisuje koliko je bilo pokušaja unosa
+            int brojPokusaja = 0;
+
+            Console.WriteLine("Unesite brojeve. Unesite broj veći od 100 za završetak.");
+
+            while (true)
+            {
+                Console.Write("Unesite broj: ");
+                string unos = Console.ReadLine();
+
+                if (int.TryParse(unos, out int broj))
+                {
+                    brojPokusaja++;
+
+                    if (broj > 100)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nevažeći unos. Molimo unesite celi broj.");
+                }
+            }
+
+            Console.WriteLine($"Broj pokušaja unosa: {brojPokusaja}");
         }
     }
 }
