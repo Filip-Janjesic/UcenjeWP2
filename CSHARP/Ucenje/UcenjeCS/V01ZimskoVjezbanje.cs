24be70a10b2da3aa97e239c220a0b170ec3503bc
Filip Janješić <filip.janjesic@gmail.com>
2023-12-27 23:57:08 +0100

    3. Napisati program koji ispisuje sve brojeve od 1 do 100 koji su cjelobrojno djeljivi s 7

--- CSHARP/Ucenje/UcenjeCS/V01ZimskoVjezbanje.cs
+++ CSHARP/Ucenje/UcenjeCS/V01ZimskoVjezbanje.cs
@@ -6,16 +6,24 @@ namespace UcenjeCS
     {
         public static void Izvedi()
         {
-            // Ispis brojeva od 1 do 100
+            // 1. Napisati program koji ispisuje sve brojeve od 1 do 100
             for (int i = 1; i <= 100; i++)
             {
                 Console.WriteLine(i);
             }
 
-            // Ispis brojeva od 100 do 1
+            // 2. Napisati program koji ispisuje sve brojeve od 100 do 1 
             for (int i = 100; i >= 1; i--)
             {
                 Console.WriteLine(i);
+            }
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 7 == 0)
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
