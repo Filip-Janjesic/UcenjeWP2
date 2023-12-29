7e57dc9901157572a7cd5ee2b8b27571125d11db
Filip Janješić <filip.janjesic@gmail.com>
2023-12-30 00:48:56 +0100

    Update V02ZimskoVjezbanje.cs

--- CSHARP/Ucenje/WebAPI/Controllers/V02ZimskoVjezbanje.cs
+++ CSHARP/Ucenje/WebAPI/Controllers/V02ZimskoVjezbanje.cs
@@ -1,12 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace WebAPI.Controllers
 {
-    public class V02ZimskoVjezbanje : Controller
+    [ApiController]
+    [Route("VO2")]
+
+    // 1. Ruta ne prima niti jedan parametar i vraća zbroj prvih 100 brojeva
+    public class V02ZimskoVjezbanje : ControllerBase
     {
-        public IActionResult Index()
+        [HttpGet]
+        [Route("vjezba1")]
+        public IActionResult Vjezba1()
         {
-            return View();
+            int zbroj = Enumerable.Range(1, 100).Sum();
+            return Ok($"Zbroj prvih 100 brojeva je: {zbroj}");
         }
     }
 }
