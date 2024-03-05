0ef98b9a266fafeba64f9236695b71acce773ed1
Filip Janješić <filip.janjesic@gmail.com>
2024-03-05 18:09:11 +0100

    Update SmjerService.js

--- Frontend/src/services/SmjerService.js
+++ Frontend/src/services/SmjerService.js
@@ -12,8 +12,32 @@ async function getSmjerovi(){
     });
 }
 
+async function obrisiSmjer(sifra){
+    return await httpService.delete('/Smjer/' + sifra)
+    .then((res)=>{
+        return {ok: true, poruka: res};
+    }).catch((e)=>{
+        console.log(e);
+    });
+}
+
+async function dodajSmjer(smjer){
+    const odgovor = await httpService.post('/Smjer',smjer)
+    .then(()=>{
+        return {ok: true, poruka: 'Uspješno dodano'}
+    })
+    .catch((e)=>{
+        //console.log(e.response.data.errors.Naziv[0]);
+        return {ok: false, poruka: e.response.data.errors.Naziv[0]}
+    });
+    return odgovor;
+}
+
+
 
 
 export default{
-    getSmjerovi
+    getSmjerovi,
+    obrisiSmjer,
+    dodajSmjer
 };
