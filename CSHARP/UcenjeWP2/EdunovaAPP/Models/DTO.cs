34deaf32e2b01089d6f16b75fc33151231694fa5
Filip Janješić <filip.janjesic@gmail.com>
2024-03-12 21:25:11 +0100

    Update DTO.cs

--- CSHARP/UcenjeWP2/EdunovaAPP/Models/DTO.cs
+++ CSHARP/UcenjeWP2/EdunovaAPP/Models/DTO.cs
@@ -1,4 +1,7 @@
-﻿namespace EdunovaAPP.Models
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System;
+
+namespace EdunovaAPP.Models
 {
     public record SmjerDTORead(int sifra, string naziv, int trajanje,
         decimal cijena, decimal upisnina, bool verificiran);
@@ -6,5 +9,45 @@
     public record SmjerDTOInsertUpdate(string naziv, int trajanje,
         decimal cijena, decimal upisnina, bool verificiran);
 
+
+
+
+
+
+    public record PolaznikDTORead(int sifra, string ime, string prezime,
+        string email, string oib, string brojugovora);
+
+    public record PolaznikDTOInsertUpdate( string ime, string prezime,
+        string email, string oib, string brojugovora);
+
+
+
+
+    public record PredavacDTORead(int sifra, string ime, string prezime,
+        string email, string oib, string iban);
+
+    public record PredavacDTOInsertUpdate(string ime, string prezime,
+        string email, string oib, string iban);
+
+    
+   
+    public record GrupaDTORead( int sifra, string naziv,
+        string smjer , string predavac ,  int brojpolaznika , DateTime? datumpocetka, int? maksimalnopolaznika);
+    /*
+    public class GrupaDTORead
+    {
+        public int? sifra { get; set; }
+        public string? naziv { get; set; }
+        public string? smjer { get; set; }
+        public string? predavac { get; set; }
+        public DateTime? datumpocetka { get; set; }
+        public int? maksimalnopolaznika { get; set; }
+        public int? brojpolaznika { get; set; }
+
+    }
+    */
+
+    public record GrupaDTOInsertUpdate( string naziv, int predavac,
+        int smjer,  DateTime? datumpocetka, int? maksimalnopolaznika= 0);
 
 }
